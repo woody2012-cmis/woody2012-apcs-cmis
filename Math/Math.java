c7bf7d488cff0904a983f886c24e22834aa18f13
cmis <cmis@cmis-hs09.(none)>
2015-08-19 11:58:14 +0700

    math

--- Math/Math.java
+++ Math/Math.java
@@ -3,9 +3,30 @@ public class Math
     {
      public static void main( String[] args )
         {
-            String input = JOptionPane.showInputDialog ( "What's your two numbers?" ); 
-            System.out.println( "Hello, " +input + "." );
+            double sum1 = 0;
+            double sum2 = 0;
+            double sub3 = 0;
+            double sub4 = 0;
             
+            String input = JOptionPane.showInputDialog ( "Give me a number" ); 
+            String input2 = JOptionPane.showInputDialog ( "Give me a second number" );
+                  
+            double no1 = Double.parseDouble (input);
+            double no2 = Double.parseDouble (input2);
+            
+            sum1 = no1 + no2;
+          
+            System.out.println( no1 + "+" + no2 + "=" + sum1 );
+            
+            sum2 = no1 + no1;
+            
+            System.out.println( no1 + "+" + no1 + "=" + sum2);
+            
+            sub3 = no1 - no2;
+            
+            System.out.println( no1 + "+" + no2 + "=" + sub3);
+            
+         
             
         } // end method main
         
