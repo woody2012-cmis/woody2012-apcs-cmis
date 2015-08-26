cbb15f709a09e046b086be57b1342ba7a3f1c790
cmis <cmis@cmis-hs10.(none)>
2015-08-26 14:34:34 +0700

    added files

---
+++ FundamentalsP5/FundamentalsP5.java
@@ -0,0 +1,15 @@
+
+public class FundamentalsP5
+    {
+     public static void main( String[] args )
+        {
+            String [] movies = {"Kingsman", "The Golden Compass", "The Great Gatsby"};
+            for(int index = 0;index<movies.length;index++)
+            {
+            System.out.println(movies[index]+ "\n"+"\n" );
+        } 
+    }
+}
+    
+    
+ 
