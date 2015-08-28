44907ccd69a9a1b3098ee5e36783db50f5570cfa
cmis <cmis@cmis-hs11.(none)>
2015-08-28 09:57:32 +0700

    added files

--- FundamentalsP5/FundamentalsP5.java
+++ FundamentalsP5/FundamentalsP5.java
@@ -3,23 +3,26 @@ public class FundamentalsP5
     {
      public static void main( String[] args )
         {
-            String [] movies = {"Kingsman", "The Golden Compass", "Spirited Away"};
+        String [] movies = {"Kingsman", "The Golden Compass", "Spirited Away"};
             
-            for(int index = 0;index<movies.length;index++)
-            {
+        for(int index = 1;index<movies.length;index++)
+        {
             System.out.println(movies[index]+ "\n"+"\n" );
         } 
         
         System.out.println("\n"+"\n");
-        String [] songs = {"Party", "Verge" , "Into the New World"};
+        
+        String [] songs = new String [] {"Party", "Verge" , "Into the New World"};
+        
+       
         
         while (int index = 0; index<songs.length;index++)
         {
-            System.out.println(songs[]);
+           
+             System.out.print(songs[index] );
         
         }
 }
 }
     
     
- 
