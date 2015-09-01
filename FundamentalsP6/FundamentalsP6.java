e834f8ee61796b6865f0188cc83c0572363ccc28
cmis <cmis@cmis-hs11.(none)>
2015-09-01 14:06:49 +0700

    added files

--- FundamentalsP6/FundamentalsP6.java
+++ FundamentalsP6/FundamentalsP6.java
@@ -3,8 +3,8 @@ public class FundamentalsP6
     {
      public static void main( String[] args )
         {
-        int [] numbers = {0, 5, 18, 21, 59, 80, 98, 100};
-        int [] randomnumbers = new int[100];
+        
+        int [] randomnumbers = new int[50];
             
        for(int index = 0;index<randomnumbers.length;index++)
         {
@@ -13,7 +13,7 @@ public class FundamentalsP6
         
        for(int index = 0;index<randomnumbers.length;index++)
         {
-            System.out.println( randomnumbers[ index] );
+            System.out.print( randomnumbers[index] + " " );
         } 
 }
 }
--- FundamentalsP7/FundamentalsP7.java
+++ FundamentalsP7/FundamentalsP7.java
@@ -3,19 +3,33 @@ public class FundamentalsP7
     {
      public static void main( String[] args )
         {
-        int [] numbers = {-100, -56, -3, -21, 0, 5, 18, 21, 98, 100};
-        int [] randomnumbers = new int[100];
+       int [] randomNumbers = new int[10];
+       int positive = 0;
+       int negative = 0;
             
-       for(int index = 0;index<randomnumbers.length;index++)
-        {
-            randomnumbers[index] =  (int) ( Math.random()*100);
-        } 
-        
-       for(int index = 0;index<randomnumbers.length;index++ + index*-1)
-        {
-            System.out.println( randomnumbers[ index] );
-        } 
+for(int index = 0;index<randomNumbers.length;index++)
+{
+    int i = 0;
+            
+    i = (int)(100*Math.random());
+    if (i % 2 == 0)
+    {
+     randomNumbers[index] = ((int)(100*(-1)*Math.random()));
+     negative++;
+    }//end if
+    else
+    {
+     randomNumbers[index] = ((int)(100*Math.random()));
+     positive++;
+    }//end else
+ 
+}//end for
+
+int index = 0;
+while (index < randomNumbers.length)
+{
+    System.out.print("There are " + positive + " numbers" + "." + "There are " + negative + " numbers" + ".");
 }
 }
-    
+}
     
