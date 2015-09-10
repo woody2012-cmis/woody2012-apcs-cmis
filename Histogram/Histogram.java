fa81d0c0c42b47533433ea6d768dd983b528ef84
cmis <cmis@cmis-hs17.(none)>
2015-09-10 11:55:59 +0700

    added files

--- Histogram/Histogram.java
+++ Histogram/Histogram.java
@@ -1,24 +1,26 @@
 import javax.swing.JOptionPane;
 public class Histogram
-    {
-     public static void main( String[] args )
-     { 
-           String input = JOptionPane.showInputDialog ( "Give me an integer." );
-           int num = Integer.parseInt (input);
-           
-           int [] randomNumbers = new int [num]; 
-   
-           for(int index=0;index<num;index++) 
-           {
-               randomNumbers [index] = (int)(Math.random()*98+1);
-               System.out.println(index + ": " + randomNumbers[index]);
-           } //end while
-           
-           
-          for(int index=0;index<num;index++) 
-           {
-               randomNumbers [index] = (int)(Math.random()*98+1);
-               System.out.println(index + ": " + randomNumbers[index]);
-           } //end while
-           } //end method main 
-     }//emg class
+{
+    public static void main( String[] args )
+    { 
+        String input = JOptionPane.showInputDialog ( "Give me an integer." );
+        int num = Integer.parseInt (input);
+
+        int [] randomNumbers = new int [num]; 
+
+        for(int index = 0; index < randomNumbers.length; index++) 
+        {
+            randomNumbers [index] = (int)(Math.random()*98+1);
+            System.out.println(index + ": " + randomNumbers[index]);
+        } //end while
+
+        int [] counts = new int [100];
+        for (int index = 0; index <= 99; index++)
+        {
+            int counter = int randomNumbers[index];
+            count[counter]++;
+
+        }
+
+    } //end method main 
+}//end class
