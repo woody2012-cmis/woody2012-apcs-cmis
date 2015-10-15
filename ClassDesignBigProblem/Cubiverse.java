5a9b00530c0c6308e8f45c0c7247662b15cf129a
cmis <cmis@cmis-hs08.(none)>
2015-10-15 09:54:28 +0700

    added files

--- ClassDesignBigProblem/Cubiverse.java
+++ ClassDesignBigProblem/Cubiverse.java
@@ -4,42 +4,73 @@ public class Cubiverse
     private int w;
     private int h;
     private int d;
-    private int v;
+    private int volume;
     private double x;
     private double y;
     private double z;
+    private double dfl;
+    private double dfbt;
+    private double dfbk;
     private String name;
 
     public Cubiverse()
     {
-        this.w = 1;
-        this.h = 1;
-        this.d = 1;
-        this.v = 1;
+        w = 1;
+        h = 1;
+        d = 1;
+        volume = w * h* d;
+        x = 0.5;
+        y = 0.5;
+        z = 0.5;
+        dfl = w * x;
+        dfbt = h * y;
+        dfbk = d * z;
+        name = "Default Cubiverse-" + Math.random ();
     }
-    
+
+    public Cubiverse(int w, int h, int d, String name)
+    {
+        this.w = w;
+        this.h = h;
+        this.d = d;
+        volume = w * h* d;
+        x = 0.5;
+        y = 0.5;
+        z = 0.5;
+        dfl = w * x;
+        dfbt = h * y;
+        dfbk = d * z;
+        this.name = name;
+    }
+
     public Cubiverse(double x, double y, double z, String name)
     {
-        this.y = y;
+        w = 1;
+        h = 1;
+        d = 1;
+        volume = w * h* d;
         this.x = x;
+        this.y = y;
         this.z = z;
-        this.name = "Name: Default Cubiverse ";
-    }//end userinout constructor 
-  
+        dfl = w * x;
+        dfbt = h * y;
+        dfbk = d * z;
+        this.name = name;
+    }
+
     public String toString()
     {
-        String output = new String();
-        output = this.name + Math.random() + 
-        "\n" + "w: " + w + 
-        "\n" + "h: " + h +
-        "\n" + "d: " + d +
-        "\n" + "volume: " + v + 
-        "\n" + "x: " + x + 
-        "\n" + "y: " + y + 
-        "\n" + "z: " + z + 
-        "\n" + "distance from left: " + x + 
-        "\n" + "distance from bottom: " + y + 
-        "\n" + "distance from back: " + z + "\n";
-        return output;
+        String result = String.format ("Name: %s \n" + 
+                "w: %d \n" +
+                "h: %d \n" +
+                "d: %d \n" +
+                "volume: %d \n" +
+                "x: %.2f \n" +
+                "y: %.2f \n" +
+                "z: %.2f \n" +
+                "distance from left: %.2f \n" +
+                "distance from bottom: %.2f \n" +
+                "distance from back: %.2f \n", name, w, h, d, volume, x, y, z, dfl, dfbt, dfbk);
+        return result;
     }
 }
