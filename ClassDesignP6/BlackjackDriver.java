4a9a5298a32d2b8ce430f82d007b400e655226ee
cmis <cmis@cmis-hs08.(none)>
2015-11-02 11:58:30 +0700

    added files

--- ClassDesignP6/BlackjackDriver.java
+++ ClassDesignP6/BlackjackDriver.java
@@ -9,53 +9,95 @@ public class BlackjackDriver
         String[] player4 = new String[2];
         String[] player5 = new String[2];
 
-        String[] ranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-        String[] suits = {"♥","♦","♣","♠"};
-        for(int i = 0; i < dealer.length; i++)
+        String[] allranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+        String[] allsuits = {"♥","♦","♣","♠"};
+        int value = 0;
+        
+        int[] ranks = new int[2]; //contains numbers in the allranks
+        int[] suits = new int[2]; //contains numbers in the allsuits
+
+         for(int i = 0; i < dealer.length; i++)
         {
-            int a = ((int) (Math.random()*12));
-            int b = ((int) (Math.random()*4));
-            dealer[i] = "Card of Dealer: " + ranks[a] + suits[b];
+            ranks[i] = ((int) (Math.random()*12));
+            suits[i] = ((int) (Math.random()*4));
+            dealer[i] = "Card of Dealer: " + allranks[ranks[i]] + allsuits[ranks[i]];
             System.out.println(dealer[i]);
         }
+        
+        for(int i = 0; i < dealer.length; i++)
+        {
+            if (dealer[i].ranks.equals("J"))
+            {
+                value = 10;
+            }
+             if (dealer[i].ranks.equals("Q"))
+            {
+                value = 10;
+            }
+             if (dealer[i].ranks.equals("K"))
+            {
+                value = 10;
+            }
+             if (dealer[i].ranks.equals("A"))
+            {
+                value = 11;
+            }
+            else 
+            {
+                int value = Integer.parseInt(ranks);
+                value = ranks;
+            }
+            System.out.println(value);
+        }
+
+  
         System.out.println("\n");
+
         for(int i = 0; i < player1.length; i++)
         {
-            int a = ((int) (Math.random()*12));
-            int b = ((int) (Math.random()*4));
-            player1[i] = "Card of Player 1: " + ranks[a] + suits[b];
+            int ranks = ((int) (Math.random()*12));
+            int suits = ((int) (Math.random()*4));
+            player1[i] = "Card of Player 1: " + allranks[ranks[i]] + allsuits[ranks[i]];
             System.out.println(player1[i]);
         }
+
         System.out.println("\n");
+
         for(int i = 0; i < player2.length; i++)
         {
-            int a = ((int) (Math.random()*12));
-            int b = ((int) (Math.random()*4));
-            player2[i] = "Card of Player 2: " + ranks[a] + suits[b];
+            int ranks = ((int) (Math.random()*12));
+            int suits = ((int) (Math.random()*4));
+            player2[i] = "Card of Player 2: " + allranks[ranks[i]] + allsuits[ranks[i]];
             System.out.println(player2[i]);
         }
+
         System.out.println("\n");
+
         for(int i = 0; i < player3.length; i++)
         {
-            int a = ((int) (Math.random()*12));
-            int b = ((int) (Math.random()*4));
-            player3[i] = "Card of Player 3: " + ranks[a] + suits[b];
+            int ranks = ((int) (Math.random()*12));
+            int suits = ((int) (Math.random()*4));
+            player3[i] = "Card of Player 3: " + allranks[ranks[i]] + allsuits[ranks[i]];
             System.out.println(player3[i]);
         }
+
         System.out.println("\n");
+
         for(int i = 0; i < player4.length; i++)
         {
-            int a = ((int) (Math.random()*12));
-            int b = ((int) (Math.random()*4));
-            player4[i] = "Card of Player 4: " + ranks[a] + suits[b];
+            int ranks = ((int) (Math.random()*12));
+            int suits = ((int) (Math.random()*4));
+            player4[i] = "Card of Player 4: " + allranks[ranks[i]] + allsuits[ranks[i]];
             System.out.println(player4[i]);
         }
+
         System.out.println("\n");
+
         for(int i = 0; i < player5.length; i++)
         {
-            int a = ((int) (Math.random()*12));
-            int b = ((int) (Math.random()*4));
-            player5[i] = "Card of Player 5: " + ranks[a] + suits[b];
+            int ranks = ((int) (Math.random()*12));
+            int suits = ((int) (Math.random()*4));
+            player5[i] = "Card of Player 5: " + allranks[ranks[i]] + allsuits[ranks[i]];
             System.out.println(player5[i]);
         }
     }//end method main
