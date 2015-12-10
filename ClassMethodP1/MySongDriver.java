6ecdf48329c0824dc314812e70e17b334fc8e68c
woody2012-cmis <woody2012@cmis.ac.th>
2015-12-10 18:38:48 +0700

    Update MySongDriver.java

--- ClassMethodP1/MySongDriver.java
+++ ClassMethodP1/MySongDriver.java
@@ -3,14 +3,13 @@ public class MySongDriver
     public static void main (String[] args)
         {
         MySong song1 = new MySong();
-        MySong song2 = new MySong("I.", "rock ballad.", 3.27);
+        
        
         
-        System.out.println(song1 + "\n" + "The song was realeased in " + song1.getyearReleased() + "." + "\n" + "\n" + song2);
+        System.out.println(song1 + "\n" + "The song was realeased in " + song1.getyearReleased() + "." + "\n" + "\n" );
         
         song1.setYearReleased(2015);
         System.out.println("The song was realeased in " + song1.getyearReleased() + "." );
         
         }
     }
-
