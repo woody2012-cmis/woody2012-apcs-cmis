3d465fc8d2b2cde6be990a61afe233c558461e30
cmis <cmis@cmis-hs09.(none)>
2016-01-14 14:32:13 +0700

    Added files

---
+++ ClassCompositionP1/MyPod.java
@@ -0,0 +1,24 @@
+
+
+public class MyPod
+{
+    // instance variables 
+    private String color;
+    private int memory;
+    private String[] songLibrary;
+
+    
+    public MyPod()
+    {
+        // initialise instance variables
+        color = new String("Yellow");
+        memory = 32;
+        songLibrary = new String[]{"Wait", "Smoke", "Drift"};
+    }
+
+    public MyPod(String color, int memory, String songLibrary)
+    {
+        // put your code here
+        return x + y;
+    }
+}
