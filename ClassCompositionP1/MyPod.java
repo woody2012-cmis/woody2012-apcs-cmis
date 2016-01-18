a2a8e18f760b86b058b14a727fb23c0c534c647d
cmis <cmis@cmis-hs07.(none)>
2016-01-18 09:51:55 +0700

    Added files

--- ClassCompositionP1/MyPod.java
+++ ClassCompositionP1/MyPod.java
@@ -1,24 +1,40 @@
-
-
 public class MyPod
 {
     // instance variables 
     private String color;
-    private int memory;
     private String[] songLibrary;
+    private int memory;
 
-    
     public MyPod()
     {
         // initialise instance variables
-        color = new String("Yellow");
-        memory = 32;
-        songLibrary = new String[]{"Wait", "Smoke", "Drift"};
+        this.color = new String("Yellow");
+        this.songLibrary = new String[3];
+        this.memory = 32;
     }
 
-    public MyPod(String color, int memory, String songLibrary)
+    public MyPod(String color, int memory)
     {
         // put your code here
-        return x + y;
+        this.color = new String(color);
+        this.memory = memory;
+        this.songLibrary = new String[3];
+        songLibrary[0] = new String("Wait");
+        songLibrary[1] = new String("Turning Tables");
+        songLibrary[2] = new String("Drift");
+    }
+
+    public String toString()
+    {
+        String output = new String();
+
+        output = color + "\n" + memory + "GB" + "\n";
+
+        for( String s : songLibrary)
+        {
+            output += s + "\n";
+        }
+        
+        return output;
     }
 }
---
+++ ClassCompositionP1/MyPodDriver.java
@@ -0,0 +1,8 @@
+public class MyPodDriver
+{
+    public static void main(String[] args)
+    {
+        MyPod carPod = new MyPod ("Yellow", 32);
+        System.out.println(carPod);
+    }
+}
