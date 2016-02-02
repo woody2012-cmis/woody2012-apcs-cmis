c809188469c806322a944bfba491dc20461d0241
cmis <cmis@cmis-hs01.(none)>
2016-02-02 14:39:39 +0700

    Added files

--- Array2D2/Array2D2.java
+++ Array2D2/Array2D2.java
@@ -5,33 +5,23 @@ public class Array2D2
         int[] [] grid = new int [3][5];
         int i = 1;
         int totalsum = 0;
-        int[] rowsum = new int[3];
-        int[] colsum = new int[5];
+        //int rowsum = 0;
+        int colsum = 0;
         for(int row = 0; row < grid.length; row++)
         {
+            int rowsum = 0;
             for(int col = 0; col < grid[0].length; col++)
             {
                 grid[row][col] = i;
                 i = i+1;
+                totalsum += grid[row][col];
+                System.out.print(grid[row][col] + "\t");
+                rowsum += grid[row][col];
             }
-        }
-
-        for(int row = 0; row < grid.length; row++)
-        {
-            for(int col = 0; col < grid[0].length; col++)
-            {
-                System.out.print(grid[row][col] + " ");
-            }
+            System.out.print("This row sum is " + rowsum + ".");
             System.out.println("\n");
         }
+        System.out.println("The total sum is " + totalsum + ".");
 
-        for(int[] row : grid)
-        {
-            for(int num : row)
-            {
-                System.out.println(rowsum);
-            }
-        }
     }
 }
-
