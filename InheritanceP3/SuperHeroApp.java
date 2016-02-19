28582325696db53dc653a790e8e63800a8b9fa8b
cmis <cmis@cmis-hs00.(none)>
2016-02-19 12:02:23 +0700

    hey

--- InheritanceP3/SuperHeroApp.java
+++ InheritanceP3/SuperHeroApp.java
@@ -3,10 +3,20 @@ public class SuperHeroApp
 {
     public static void main(String[] args)
     {
-
         AsteriodMan asteriodman = new AsteriodMan();
+        asteriodman.isCaped();
+        asteriodman.setCape(false);
+        asteriodman.setSuitColor("Purple");
+
         FriedEggMan friedeggman = new FriedEggMan();
+        asteriodman.isCaped();
+        asteriodman.setCape(true);
+        friedeggman.setSuitColor("Yellow");
+
         PacMan pacman = new PacMan();
+        asteriodman.isCaped();
+        asteriodman.setCape(false);
+        pacman.setSuitColor("Blue");
 
         ArrayList<SuperHero> heroes = new ArrayList<SuperHero>();
         heroes.add(asteriodman);
