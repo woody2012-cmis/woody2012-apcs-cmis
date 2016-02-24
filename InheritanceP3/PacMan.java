18bfee4c98dbc3f53d9e5382f266ef6eb3fda9fd
cmis <cmis@cmis-hs01.(none)>
2016-02-24 09:14:26 +0700

    added files

--- InheritanceP3/PacMan.java
+++ InheritanceP3/PacMan.java
@@ -18,3 +18,4 @@ public class PacMan extends SuperHero
         return "PacMan\t" + super.toString();
     }
 }
+
--- InheritanceP3/SuperHeroApp.java
+++ InheritanceP3/SuperHeroApp.java
@@ -25,6 +25,6 @@ public class SuperHeroApp
         for ( SuperHero superhero : heroes )
         {
             System.out.println(superhero + "\n");
-        }     
+        }
     }
 }
